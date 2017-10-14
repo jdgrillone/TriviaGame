050c2702d66cecf22493a20b391d177d09a39fac
jdgrillone <jdgrillone@gmail.com>
2017-10-14 14:44:08 -0600

    index.html populated, started psudocoding app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+//Game Array Object Holding all info for questions
+var gameArray = {
+	questionOne: {
+		question: "What color is my cat?"
+		possible: ["Black", "White", "Green", "Blue"]
+		correct: "Black"
+	}
+};
+//Global Variables
+var correctAnswers = 0;
+var wrongAnswers = 0;
+
+//Game Start Function(q)
+	//Display question to Document
+	//Display buttons for possible answers with onclick listeners.
+		//for (i = 0; i < 4, i++)
+		
+		//$("#buttons").html(<button>)
+	//Start timer for 30 seconds
+	//If answer === correct
+		//Display congratulation message
+		//correctAnswers++
+	//Else
+		//Display correct answer
+		//wrongAnswers++
+
+
